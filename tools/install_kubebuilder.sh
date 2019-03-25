68122b55a68e1a0588585c57599019e7e0748746
Michael Hrivnak <mhrivnak@redhat.com>
2019-03-25 10:35:42 -0400

    installs kubebuilder locally instead of globally

--- tools/install_kubebuilder.sh
+++ tools/install_kubebuilder.sh
@@ -6,6 +6,5 @@ arch=amd64
 curl -L -O "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${version}/kubebuilder_${version}_linux_${arch}.tar.gz"
 
 tar -zxvf kubebuilder_${version}_linux_${arch}.tar.gz
-mv kubebuilder_${version}_linux_${arch} kubebuilder && sudo mv kubebuilder /usr/local/
 
-export PATH=$PATH:/usr/local/kubebuilder/bin
+mv kubebuilder_${version}_linux_${arch} kubebuilder
