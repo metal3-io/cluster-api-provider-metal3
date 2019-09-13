c951a12929d223484cc0a478ee4de2655fcf4f68
Arvinderpal Wander <awander@gmail.com>
2019-09-13 08:20:46 -0700

    Added skeleton BareMetalMachine type. Includes kubebuilder boilerplate code for BareMetalMachine.

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	infrastructurev1alpha2 "github.com/metal3-io/cluster-api-provider-baremetal/api/v1alpha2"
+	"github.com/metal3-io/cluster-api-provider-baremetal/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = infrastructurev1alpha2.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -58,6 +61,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.BareMetalMachineReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("BareMetalMachine"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "BareMetalMachine")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
