85319631199c6f01e1fd784c4aa9fd717623de87
Russell Bryant <rbryant@redhat.com>
2019-10-10 11:41:55 -0400

    Create and use a unit test image.
    Instead of installing kubebuilder and kustomize each time the unit
    tests run in a container, just pre-install those in a container image.
    
    Also update install_kubebuilder.sh to remove the downloaded tar.gz to
    reduce the size of the resulting container image.

--- hack/unit.sh
+++ hack/unit.sh
@@ -5,19 +5,15 @@ set -eux
 IS_CONTAINER=${IS_CONTAINER:-false}
 
 if [ "${IS_CONTAINER}" != "false" ]; then
-  TOP_DIR="${1:-$(pwd)}"
-  cd
-  "${TOP_DIR}"/tools/install_kustomize.sh
-  "${TOP_DIR}"/tools/install_kubebuilder.sh
-  mv kubebuilder /usr/local/.
-  cd "${TOP_DIR}"
+  eval "$(go env)"
+  cd "${GOPATH}"/src/github.com/metal3-io/cluster-api-provider-baremetal
   go test ./pkg/... ./cmd/... -coverprofile /cover.out
 else
   podman run --rm \
     --env IS_CONTAINER=TRUE \
-    --volume "${PWD}:/go/src/github.com/metal3-io/cluster-api-provider-baremetal:ro,z" \
+    --volume "${PWD}:/root/go/src/github.com/metal3-io/cluster-api-provider-baremetal:ro,z" \
     --entrypoint sh \
-    --workdir /go/src/github.com/metal3-io/cluster-api-provider-baremetal \
-    registry.hub.docker.com/library/golang:1.12 \
-    /go/src/github.com/metal3-io/cluster-api-provider-baremetal/hack/unit.sh "${@}"
+    --workdir /root/go/src/github.com/metal3-io/cluster-api-provider-baremetal \
+    quay.io/metal3-io/capbm-unit \
+    /root/go/src/github.com/metal3-io/cluster-api-provider-baremetal/hack/unit.sh "${@}"
 fi;
