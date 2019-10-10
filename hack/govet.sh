21d0ba95d2a02ca79a783db1b481594d7af79feb
maelk <mael.kimmerlin@est.tech>
2019-10-10 08:46:16 +0300

    Add test scripts in hack and remove aws specifics

---
+++ hack/govet.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eux
+
+IS_CONTAINER=${IS_CONTAINER:-false}
+
+if [ "${IS_CONTAINER}" != "false" ]; then
+  TOP_DIR="${1:-.}"
+  go vet "${TOP_DIR}"/pkg/... "${TOP_DIR}"/cmd/...
+else
+  podman run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/metal3-io/cluster-api-provider-baremetal:ro,z" \
+    --entrypoint sh \
+    --workdir /go/src/github.com/metal3-io/cluster-api-provider-baremetal \
+    registry.hub.docker.com/library/golang:1.12 \
+    /go/src/github.com/metal3-io/cluster-api-provider-baremetal/hack/govet.sh "${@}"
+fi;
