b0d3faaddbe97973e189a5b59eaffaf73421fbe8
maelk <mael.kimmerlin@est.tech>
2019-10-17 09:38:33 +0300

    V1alpha2 Use CAPI release-0.2 branch to avoid v1alpha3 issues
    Co-authored-by: Jan Tilles <jan.tilles@est.tech>

--- hack/tools/install_kustomize.sh
+++ hack/tools/install_kustomize.sh
@@ -3,5 +3,5 @@
 [[ -f bin/kustomize ]] && exit 0
 
 mkdir -p ./bin
-curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64 -o ./bin/kustomize
+curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.2.3/kustomize_kustomize.v3.2.3_linux_amd64 -o ./bin/kustomize
 chmod +x ./bin/kustomize
