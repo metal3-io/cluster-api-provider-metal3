metal3-io/cluster-api-provider-baremetal

Rename baremetal CRDs into Metal3 CRDs
and rename files to follow the naming conventions

baremetal/metal3cluster_manager.go
@@ -1,0 +1,220 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package baremetal
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+
+	// TODO Why blank import ?
+	_ "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	capm3 "github.com/metal3-io/cluster-api-provider-baremetal/api/v1alpha3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+	capierrors "sigs.k8s.io/cluster-api/errors"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ClusterManagerInterface is an interface for a ClusterManager
+type ClusterManagerInterface interface {
+	Create(context.Context) error
+	Delete() error
+	UpdateClusterStatus() error
+	SetFinalizer()
+	UnsetFinalizer()
+	CountDescendants(context.Context) (int, error)
+}
+
+// ClusterManager is responsible for performing machine reconciliation
+type ClusterManager struct {
+	client client.Client
+
+	Cluster          *capi.Cluster
+	Metal3Cluster *capm3.Metal3Cluster
+	Log              logr.Logger
+	// name string
+}
+
+// NewClusterManager returns a new helper for managing a cluster with a given name.
+func NewClusterManager(client client.Client, cluster *capi.Cluster,
+	metal3Cluster *capm3.Metal3Cluster,
+	clusterLog logr.Logger) (ClusterManagerInterface, error) {
+
+	if metal3Cluster == nil {
+		return nil, errors.New("Metal3Cluster is required when creating a ClusterManager")
+	}
+	if cluster == nil {
+		return nil, errors.New("Cluster is required when creating a ClusterManager")
+	}
+
+	return &ClusterManager{
+		client:           client,
+		Metal3Cluster: metal3Cluster,
+		Cluster:          cluster,
+		Log:              clusterLog,
+	}, nil
+}
+
+// SetFinalizer sets finalizer
+func (s *ClusterManager) SetFinalizer() {
+	// If the Metal3Cluster doesn't have finalizer, add it.
+	if !util.Contains(s.Metal3Cluster.ObjectMeta.Finalizers, capm3.ClusterFinalizer) {
+		s.Metal3Cluster.ObjectMeta.Finalizers = append(
+			s.Metal3Cluster.ObjectMeta.Finalizers, capm3.ClusterFinalizer,
+		)
+	}
+}
+
+// UnsetFinalizer unsets finalizer
+func (s *ClusterManager) UnsetFinalizer() {
+	// Cluster is deleted so remove the finalizer.
+	s.Metal3Cluster.ObjectMeta.Finalizers = util.Filter(
+		s.Metal3Cluster.ObjectMeta.Finalizers, capm3.ClusterFinalizer,
+	)
+}
+
+// Create creates a cluster manager for the cluster.
+func (s *ClusterManager) Create(ctx context.Context) error {
+
+	config := s.Metal3Cluster.Spec
+	err := config.IsValid()
+	if err != nil {
+		// Should have been picked earlier. Do not requeue
+		s.setError("Invalid Metal3Cluster provided", capierrors.InvalidConfigurationClusterError)
+		return err
+	}
+
+	// clear an error if one was previously set
+	s.clearError()
+
+	return nil
+}
+
+// ControlPlaneEndpoint returns cluster controlplane endpoint
+func (s *ClusterManager) ControlPlaneEndpoint() ([]capm3.APIEndpoint, error) {
+	//Get IP address from spec, which gets it from posted cr yaml
+	endPoint := s.Metal3Cluster.Spec.ControlPlaneEndpoint
+	var err error
+
+	if endPoint.Host == "" || endPoint.Port == 0 {
+		s.Log.Error(err, "Host IP or PORT not set")
+		return nil, err
+	}
+
+	return []capm3.APIEndpoint{
+		{
+			Host: endPoint.Host,
+			Port: endPoint.Port,
+		},
+	}, nil
+}
+
+// Delete function, no-op for now
+func (s *ClusterManager) Delete() error {
+	return nil
+}
+
+// UpdateClusterStatus updates a machine object's status.
+func (s *ClusterManager) UpdateClusterStatus() error {
+
+	// Get APIEndpoints from  metal3Cluster Spec
+	_, err := s.ControlPlaneEndpoint()
+
+	if err != nil {
+		s.Metal3Cluster.Status.Ready = false
+		s.setError("Invalid ControlPlaneEndpoint values", capierrors.InvalidConfigurationClusterError)
+		return err
+	}
+
+	// Mark the metal3Cluster ready
+	s.Metal3Cluster.Status.Ready = true
+	now := metav1.Now()
+	s.Metal3Cluster.Status.LastUpdated = &now
+	return nil
+}
+
+// setError sets the FailureMessage and FailureReason fields on the machine and logs
+// the message. It assumes the reason is invalid configuration, since that is
+// currently the only relevant MachineStatusError choice.
+func (s *ClusterManager) setError(message string, reason capierrors.ClusterStatusError) {
+	s.Metal3Cluster.Status.FailureMessage = &message
+	s.Metal3Cluster.Status.FailureReason = &reason
+}
+
+// clearError removes the ErrorMessage from the machine's Status if set. Returns
+// nil if ErrorMessage was already nil. Returns a RequeueAfterError if the
+// machine was updated.
+func (s *ClusterManager) clearError() {
+	if s.Metal3Cluster.Status.FailureMessage != nil || s.Metal3Cluster.Status.FailureReason != nil {
+		s.Metal3Cluster.Status.FailureMessage = nil
+		s.Metal3Cluster.Status.FailureReason = nil
+	}
+}
+
+// CountDescendants will return the number of descendants objects of the
+// metal3Cluster
+func (s *ClusterManager) CountDescendants(ctx context.Context) (int, error) {
+	// Verify that no metal3machine depend on the metal3cluster
+	descendants, err := s.listDescendants(ctx)
+	if err != nil {
+		s.Log.Error(err, "Failed to list descendants")
+
+		return 0, err
+	}
+
+	nbDescendants := len(descendants.Items)
+
+	if nbDescendants > 0 {
+		s.Log.Info(
+			"metal3Cluster still has descendants - need to requeue", "descendants",
+			nbDescendants,
+		)
+	}
+	return nbDescendants, nil
+}
+
+// listDescendants returns a list of all Machines, for the cluster owning the
+// metal3Cluster.
+func (s *ClusterManager) listDescendants(ctx context.Context) (capi.MachineList, error) {
+
+	machines := capi.MachineList{}
+	cluster, err := util.GetOwnerCluster(ctx, s.client,
+		s.Metal3Cluster.ObjectMeta,
+	)
+	if err != nil {
+		return machines, err
+	}
+
+	listOptions := []client.ListOption{
+		client.InNamespace(cluster.Namespace),
+		client.MatchingLabels(map[string]string{
+			capi.ClusterLabelName: cluster.Name,
+		}),
+	}
+
+	if s.client.List(ctx, &machines, listOptions...) != nil {
+		errMsg := fmt.Sprintf("failed to list metal3machines for cluster %s/%s", cluster.Namespace, cluster.Name)
+		return machines, errors.Wrapf(err, errMsg)
+	}
+
+	return machines, nil
+}

controllers/metal3cluster_controller.go
@@ -1,0 +1,182 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+
+	capm3 "github.com/metal3-io/cluster-api-provider-baremetal/api/v1alpha3"
+	"github.com/metal3-io/cluster-api-provider-baremetal/baremetal"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/utils/pointer"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+	capierrors "sigs.k8s.io/cluster-api/errors"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/patch"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	clusterControllerName = "Metal3Cluster-controller"
+	requeueAfter          = time.Second * 30
+)
+
+// Metal3ClusterReconciler reconciles a Metal3Cluster object
+type Metal3ClusterReconciler struct {
+	Client         client.Client
+	ManagerFactory baremetal.ManagerFactoryInterface
+	Log            logr.Logger
+}
+
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=metal3clusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=metal3clusters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+
+// Reconcile reads that state of the cluster for a Metal3Cluster object and makes changes based on the state read
+// and what is in the Metal3Cluster.Spec
+func (r *Metal3ClusterReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, rerr error) {
+
+	ctx := context.Background()
+	clusterLog := log.Log.WithName(clusterControllerName).WithValues("baremetal-cluster", req.NamespacedName)
+
+	// Fetch the Metal3Cluster instance
+	metal3Cluster := &capm3.Metal3Cluster{}
+
+	if err := r.Client.Get(ctx, req.NamespacedName, metal3Cluster); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{}, err
+	}
+
+	helper, err := patch.NewHelper(metal3Cluster, r.Client)
+	if err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to init patch helper")
+	}
+	// Always patch metal3Cluster when exiting this function so we can persist any metal3Cluster changes.
+	defer func() {
+		err := helper.Patch(ctx, metal3Cluster)
+		if err != nil {
+			clusterLog.Error(err, "failed to Patch metal3Cluster")
+		}
+	}()
+
+	// Fetch the Cluster.
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, metal3Cluster.ObjectMeta)
+	if err != nil {
+		error := capierrors.InvalidConfigurationClusterError
+		metal3Cluster.Status.FailureReason = &error
+		metal3Cluster.Status.FailureMessage = pointer.StringPtr("Unable to get owner cluster")
+		return ctrl.Result{}, err
+	}
+	if cluster == nil {
+		clusterLog.Info("Waiting for Cluster Controller to set OwnerRef on Metal3Cluster")
+		return ctrl.Result{}, nil
+	}
+
+	clusterLog = clusterLog.WithValues("cluster", cluster.Name)
+
+	// Return early if BMCluster or Cluster is paused.
+	if util.IsPaused(cluster, metal3Cluster) {
+		clusterLog.Info("reconciliation is paused for this object")
+		return ctrl.Result{Requeue: true, RequeueAfter: requeueAfter}, nil
+	}
+
+	clusterLog.Info("Reconciling metal3Cluster")
+
+	// Create a helper for managing a Metal3 cluster.
+	clusterMgr, err := r.ManagerFactory.NewClusterManager(cluster, metal3Cluster, clusterLog)
+	if err != nil {
+		return ctrl.Result{}, errors.Wrapf(err, "failed to create helper for managing the clusterMgr")
+	}
+	if clusterMgr == nil {
+		return ctrl.Result{}, nil
+	}
+
+	// Handle deleted clusters
+	if !metal3Cluster.DeletionTimestamp.IsZero() {
+		return reconcileDelete(ctx, clusterMgr)
+	}
+
+	// Handle non-deleted clusters
+	return reconcileNormal(ctx, clusterMgr)
+}
+
+func reconcileNormal(ctx context.Context, clusterMgr baremetal.ClusterManagerInterface) (ctrl.Result, error) {
+	// If the Metal3Cluster doesn't have finalizer, add it.
+	clusterMgr.SetFinalizer()
+
+	//Create the Metal3 cluster (no-op)
+	if err := clusterMgr.Create(ctx); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Set APIEndpoints so the Cluster API Cluster Controller can pull it
+	if err := clusterMgr.UpdateClusterStatus(); err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to get ip for the API endpoint")
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func reconcileDelete(ctx context.Context,
+	clusterMgr baremetal.ClusterManagerInterface) (ctrl.Result, error) {
+
+	// Verify that no metal3machine depend on the metal3cluster
+	descendants, err := clusterMgr.CountDescendants(ctx)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if descendants > 0 {
+		// Requeue so we can check the next time to see if there are still any
+		// descendants left.
+		return ctrl.Result{Requeue: true, RequeueAfter: requeueAfter}, nil
+	}
+
+	if err := clusterMgr.Delete(); err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to delete Metal3Cluster")
+	}
+
+	// Cluster is deleted so remove the finalizer.
+	clusterMgr.UnsetFinalizer()
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager will add watches for this controller
+func (r *Metal3ClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&capm3.Metal3Cluster{}).
+		Watches(
+			&source.Kind{Type: &capi.Cluster{}},
+			&handler.EnqueueRequestsFromMapFunc{
+				ToRequests: util.ClusterToInfrastructureMapFunc(
+					capm3.GroupVersion.WithKind("Metal3Cluster"),
+				),
+			},
+		).
+		Complete(r)
+}

metal3-io/cluster-api-provider-metal3

baremetal/metal3cluster_manager.go
@@ -1,0 +1,220 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package baremetal
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+
+	// TODO Why blank import ?
+	_ "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	capm3 "github.com/metal3-io/cluster-api-provider-metal3/api/v1alpha3"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+	capierrors "sigs.k8s.io/cluster-api/errors"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ClusterManagerInterface is an interface for a ClusterManager
+type ClusterManagerInterface interface {
+	Create(context.Context) error
+	Delete() error
+	UpdateClusterStatus() error
+	SetFinalizer()
+	UnsetFinalizer()
+	CountDescendants(context.Context) (int, error)
+}
+
+// ClusterManager is responsible for performing machine reconciliation
+type ClusterManager struct {
+	client client.Client
+
+	Cluster       *capi.Cluster
+	Metal3Cluster *capm3.Metal3Cluster
+	Log           logr.Logger
+	// name string
+}
+
+// NewClusterManager returns a new helper for managing a cluster with a given name.
+func NewClusterManager(client client.Client, cluster *capi.Cluster,
+	metal3Cluster *capm3.Metal3Cluster,
+	clusterLog logr.Logger) (ClusterManagerInterface, error) {
+
+	if metal3Cluster == nil {
+		return nil, errors.New("Metal3Cluster is required when creating a ClusterManager")
+	}
+	if cluster == nil {
+		return nil, errors.New("Cluster is required when creating a ClusterManager")
+	}
+
+	return &ClusterManager{
+		client:        client,
+		Metal3Cluster: metal3Cluster,
+		Cluster:       cluster,
+		Log:           clusterLog,
+	}, nil
+}
+
+// SetFinalizer sets finalizer
+func (s *ClusterManager) SetFinalizer() {
+	// If the Metal3Cluster doesn't have finalizer, add it.
+	if !util.Contains(s.Metal3Cluster.ObjectMeta.Finalizers, capm3.ClusterFinalizer) {
+		s.Metal3Cluster.ObjectMeta.Finalizers = append(
+			s.Metal3Cluster.ObjectMeta.Finalizers, capm3.ClusterFinalizer,
+		)
+	}
+}
+
+// UnsetFinalizer unsets finalizer
+func (s *ClusterManager) UnsetFinalizer() {
+	// Cluster is deleted so remove the finalizer.
+	s.Metal3Cluster.ObjectMeta.Finalizers = util.Filter(
+		s.Metal3Cluster.ObjectMeta.Finalizers, capm3.ClusterFinalizer,
+	)
+}
+
+// Create creates a cluster manager for the cluster.
+func (s *ClusterManager) Create(ctx context.Context) error {
+
+	config := s.Metal3Cluster.Spec
+	err := config.IsValid()
+	if err != nil {
+		// Should have been picked earlier. Do not requeue
+		s.setError("Invalid Metal3Cluster provided", capierrors.InvalidConfigurationClusterError)
+		return err
+	}
+
+	// clear an error if one was previously set
+	s.clearError()
+
+	return nil
+}
+
+// ControlPlaneEndpoint returns cluster controlplane endpoint
+func (s *ClusterManager) ControlPlaneEndpoint() ([]capm3.APIEndpoint, error) {
+	//Get IP address from spec, which gets it from posted cr yaml
+	endPoint := s.Metal3Cluster.Spec.ControlPlaneEndpoint
+	var err error
+
+	if endPoint.Host == "" || endPoint.Port == 0 {
+		s.Log.Error(err, "Host IP or PORT not set")
+		return nil, err
+	}
+
+	return []capm3.APIEndpoint{
+		{
+			Host: endPoint.Host,
+			Port: endPoint.Port,
+		},
+	}, nil
+}
+
+// Delete function, no-op for now
+func (s *ClusterManager) Delete() error {
+	return nil
+}
+
+// UpdateClusterStatus updates a machine object's status.
+func (s *ClusterManager) UpdateClusterStatus() error {
+
+	// Get APIEndpoints from  metal3Cluster Spec
+	_, err := s.ControlPlaneEndpoint()
+
+	if err != nil {
+		s.Metal3Cluster.Status.Ready = false
+		s.setError("Invalid ControlPlaneEndpoint values", capierrors.InvalidConfigurationClusterError)
+		return err
+	}
+
+	// Mark the metal3Cluster ready
+	s.Metal3Cluster.Status.Ready = true
+	now := metav1.Now()
+	s.Metal3Cluster.Status.LastUpdated = &now
+	return nil
+}
+
+// setError sets the FailureMessage and FailureReason fields on the machine and logs
+// the message. It assumes the reason is invalid configuration, since that is
+// currently the only relevant MachineStatusError choice.
+func (s *ClusterManager) setError(message string, reason capierrors.ClusterStatusError) {
+	s.Metal3Cluster.Status.FailureMessage = &message
+	s.Metal3Cluster.Status.FailureReason = &reason
+}
+
+// clearError removes the ErrorMessage from the machine's Status if set. Returns
+// nil if ErrorMessage was already nil. Returns a RequeueAfterError if the
+// machine was updated.
+func (s *ClusterManager) clearError() {
+	if s.Metal3Cluster.Status.FailureMessage != nil || s.Metal3Cluster.Status.FailureReason != nil {
+		s.Metal3Cluster.Status.FailureMessage = nil
+		s.Metal3Cluster.Status.FailureReason = nil
+	}
+}
+
+// CountDescendants will return the number of descendants objects of the
+// metal3Cluster
+func (s *ClusterManager) CountDescendants(ctx context.Context) (int, error) {
+	// Verify that no metal3machine depend on the metal3cluster
+	descendants, err := s.listDescendants(ctx)
+	if err != nil {
+		s.Log.Error(err, "Failed to list descendants")
+
+		return 0, err
+	}
+
+	nbDescendants := len(descendants.Items)
+
+	if nbDescendants > 0 {
+		s.Log.Info(
+			"metal3Cluster still has descendants - need to requeue", "descendants",
+			nbDescendants,
+		)
+	}
+	return nbDescendants, nil
+}
+
+// listDescendants returns a list of all Machines, for the cluster owning the
+// metal3Cluster.
+func (s *ClusterManager) listDescendants(ctx context.Context) (capi.MachineList, error) {
+
+	machines := capi.MachineList{}
+	cluster, err := util.GetOwnerCluster(ctx, s.client,
+		s.Metal3Cluster.ObjectMeta,
+	)
+	if err != nil {
+		return machines, err
+	}
+
+	listOptions := []client.ListOption{
+		client.InNamespace(cluster.Namespace),
+		client.MatchingLabels(map[string]string{
+			capi.ClusterLabelName: cluster.Name,
+		}),
+	}
+
+	if s.client.List(ctx, &machines, listOptions...) != nil {
+		errMsg := fmt.Sprintf("failed to list metal3machines for cluster %s/%s", cluster.Namespace, cluster.Name)
+		return machines, errors.Wrapf(err, errMsg)
+	}
+
+	return machines, nil
+}

controllers/metal3cluster_controller.go
@@ -1,0 +1,182 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+
+	capm3 "github.com/metal3-io/cluster-api-provider-metal3/api/v1alpha3"
+	"github.com/metal3-io/cluster-api-provider-metal3/baremetal"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/utils/pointer"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+	capierrors "sigs.k8s.io/cluster-api/errors"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/patch"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	clusterControllerName = "Metal3Cluster-controller"
+	requeueAfter          = time.Second * 30
+)
+
+// Metal3ClusterReconciler reconciles a Metal3Cluster object
+type Metal3ClusterReconciler struct {
+	Client         client.Client
+	ManagerFactory baremetal.ManagerFactoryInterface
+	Log            logr.Logger
+}
+
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=metal3clusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=metal3clusters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters;clusters/status,verbs=get;list;watch
+
+// Reconcile reads that state of the cluster for a Metal3Cluster object and makes changes based on the state read
+// and what is in the Metal3Cluster.Spec
+func (r *Metal3ClusterReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, rerr error) {
+
+	ctx := context.Background()
+	clusterLog := log.Log.WithName(clusterControllerName).WithValues("metal3-cluster", req.NamespacedName)
+
+	// Fetch the Metal3Cluster instance
+	metal3Cluster := &capm3.Metal3Cluster{}
+
+	if err := r.Client.Get(ctx, req.NamespacedName, metal3Cluster); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{}, err
+	}
+
+	helper, err := patch.NewHelper(metal3Cluster, r.Client)
+	if err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to init patch helper")
+	}
+	// Always patch metal3Cluster when exiting this function so we can persist any metal3Cluster changes.
+	defer func() {
+		err := helper.Patch(ctx, metal3Cluster)
+		if err != nil {
+			clusterLog.Error(err, "failed to Patch metal3Cluster")
+		}
+	}()
+
+	// Fetch the Cluster.
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, metal3Cluster.ObjectMeta)
+	if err != nil {
+		error := capierrors.InvalidConfigurationClusterError
+		metal3Cluster.Status.FailureReason = &error
+		metal3Cluster.Status.FailureMessage = pointer.StringPtr("Unable to get owner cluster")
+		return ctrl.Result{}, err
+	}
+	if cluster == nil {
+		clusterLog.Info("Waiting for Cluster Controller to set OwnerRef on Metal3Cluster")
+		return ctrl.Result{}, nil
+	}
+
+	clusterLog = clusterLog.WithValues("cluster", cluster.Name)
+
+	// Return early if BMCluster or Cluster is paused.
+	if util.IsPaused(cluster, metal3Cluster) {
+		clusterLog.Info("reconciliation is paused for this object")
+		return ctrl.Result{Requeue: true, RequeueAfter: requeueAfter}, nil
+	}
+
+	clusterLog.Info("Reconciling metal3Cluster")
+
+	// Create a helper for managing a Metal3 cluster.
+	clusterMgr, err := r.ManagerFactory.NewClusterManager(cluster, metal3Cluster, clusterLog)
+	if err != nil {
+		return ctrl.Result{}, errors.Wrapf(err, "failed to create helper for managing the clusterMgr")
+	}
+	if clusterMgr == nil {
+		return ctrl.Result{}, nil
+	}
+
+	// Handle deleted clusters
+	if !metal3Cluster.DeletionTimestamp.IsZero() {
+		return reconcileDelete(ctx, clusterMgr)
+	}
+
+	// Handle non-deleted clusters
+	return reconcileNormal(ctx, clusterMgr)
+}
+
+func reconcileNormal(ctx context.Context, clusterMgr baremetal.ClusterManagerInterface) (ctrl.Result, error) {
+	// If the Metal3Cluster doesn't have finalizer, add it.
+	clusterMgr.SetFinalizer()
+
+	//Create the Metal3 cluster (no-op)
+	if err := clusterMgr.Create(ctx); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Set APIEndpoints so the Cluster API Cluster Controller can pull it
+	if err := clusterMgr.UpdateClusterStatus(); err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to get ip for the API endpoint")
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func reconcileDelete(ctx context.Context,
+	clusterMgr baremetal.ClusterManagerInterface) (ctrl.Result, error) {
+
+	// Verify that no metal3machine depend on the metal3cluster
+	descendants, err := clusterMgr.CountDescendants(ctx)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if descendants > 0 {
+		// Requeue so we can check the next time to see if there are still any
+		// descendants left.
+		return ctrl.Result{Requeue: true, RequeueAfter: requeueAfter}, nil
+	}
+
+	if err := clusterMgr.Delete(); err != nil {
+		return ctrl.Result{}, errors.Wrap(err, "failed to delete Metal3Cluster")
+	}
+
+	// Cluster is deleted so remove the finalizer.
+	clusterMgr.UnsetFinalizer()
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager will add watches for this controller
+func (r *Metal3ClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&capm3.Metal3Cluster{}).
+		Watches(
+			&source.Kind{Type: &capi.Cluster{}},
+			&handler.EnqueueRequestsFromMapFunc{
+				ToRequests: util.ClusterToInfrastructureMapFunc(
+					capm3.GroupVersion.WithKind("Metal3Cluster"),
+				),
+			},
+		).
+		Complete(r)
+}