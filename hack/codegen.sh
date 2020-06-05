6478af93bdc58f97be3ff3cc5a324629c6800de5
digambar15 <digambarpat@gmail.com>
2020-06-05 04:59:42 -0500

    Fix shellcheck failures

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -7,14 +7,16 @@ ARTIFACTS=${ARTIFACTS:-/tmp}
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
 
 if [ "${IS_CONTAINER}" != "false" ]; then
+  export XDG_CACHE_HOME=/tmp/.cache
   eval "$(go env)"
   cd "${GOPATH}"/src/github.com/metal3-io/cluster-api-provider-metal3
-  export XDG_CACHE_HOME=/tmp/.cache
   INPUT_FILES="api/v1alpha2/zz_generated.*.go api/v1alpha3/zz_generated.*.go api/v1alpha4/zz_generated.*.go baremetal/mocks/zz_generated.*.go"
-  cksum "$INPUT_FILES" > "$ARTIFACTS/lint.cksums.before"
+  # shellcheck disable=SC2086
+  cksum $INPUT_FILES > "$ARTIFACTS/lint.cksums.before"
   export VERBOSE="--verbose"
   make generate
-  cksum "$INPUT_FILES" > "$ARTIFACTS/lint.cksums.after"
+  # shellcheck disable=SC2086
+  cksum $INPUT_FILES > "$ARTIFACTS/lint.cksums.after"
   diff "$ARTIFACTS/lint.cksums.before" "$ARTIFACTS/lint.cksums.after"
 else
   "${CONTAINER_RUNTIME}" run --rm \
