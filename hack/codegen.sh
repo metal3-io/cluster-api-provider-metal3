1ec388fc5dbf125aba11a5a8b47d9b076cc34153
maelk <mael.kimmerlin@est.tech>
2020-06-15 09:24:41 +0300

    Update test scripts
    Adds golint.sh and codegen.sh and make gofmt.sh fail if files
    are changed

---
+++ hack/codegen.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -eux
+
+IS_CONTAINER="${IS_CONTAINER:-false}"
+ARTIFACTS="${ARTIFACTS:-/tmp}"
+CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
+
+if [ "${IS_CONTAINER}" != "false" ]; then
+  export XDG_CACHE_HOME=/tmp/.cache
+  eval "$(go env)"
+  cd "${GOPATH}"/src/github.com/metal3-io/cluster-api-provider-metal3
+  INPUT_FILES="\
+  config/certmanager/*.yaml
+  config/crd/*.yaml
+  config/crd/bases/*.yaml
+  config/crd/patches/*.yaml
+  config/default/*.yaml
+  config/manager/*.yaml
+  config/rbac/*.yaml
+  config/webhook/*.yaml
+  config/*.yaml
+  api/v1alpha2/zz_generated.*.go
+  api/v1alpha3/zz_generated.*.go
+  baremetal/mocks/zz_generated.*.go"
+
+  # shellcheck disable=SC2086
+  cksum $INPUT_FILES > "$ARTIFACTS/lint.cksums.before"
+  export VERBOSE="--verbose"
+  make generate
+  # shellcheck disable=SC2086
+  cksum $INPUT_FILES > "$ARTIFACTS/lint.cksums.after"
+  diff "$ARTIFACTS/lint.cksums.before" "$ARTIFACTS/lint.cksums.after"
+else
+  "${CONTAINER_RUNTIME}" run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/metal3-io/cluster-api-provider-metal3:rw,z" \
+    --entrypoint sh \
+    --workdir /go/src/github.com/metal3-io/cluster-api-provider-metal3 \
+    registry.hub.docker.com/library/golang:1.13.7 \
+    /go/src/github.com/metal3-io/cluster-api-provider-metal3/hack/codegen.sh
+fi;
--- hack/gofmt.sh
+++ hack/gofmt.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-IS_CONTAINER=${IS_CONTAINER:-false}
+IS_CONTAINER="${IS_CONTAINER:-false}"
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
 
 if [ "${IS_CONTAINER}" != "false" ]; then
@@ -10,13 +10,18 @@ if [ "${IS_CONTAINER}" != "false" ]; then
   mkdir /tmp/unit
   cp -r ./* /tmp/unit
   cd /tmp/unit
-  make fmt
+  make fmt > /tmp/fmt-output.log
+  FILE_LENGTH="$(wc -l /tmp/fmt-output.log | awk '{ print $1 }')"
+  cat /tmp/fmt-output.log
+  if [ "${FILE_LENGTH}" != "1" ]; then
+    exit 1
+  fi
 else
   "${CONTAINER_RUNTIME}" run --rm \
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/metal3-io/cluster-api-provider-metal3:ro,z" \
     --entrypoint sh \
     --workdir /go/src/github.com/metal3-io/cluster-api-provider-metal3 \
-    registry.hub.docker.com/library/golang:1.12 \
+    registry.hub.docker.com/library/golang:1.13.7 \
     /go/src/github.com/metal3-io/cluster-api-provider-metal3/hack/gofmt.sh
 fi;
