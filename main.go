181751a9b9fb55094cacf5d8213f4bc0d48fb72e
Furkat Gofurov <furkat.gofurov@est.tech>
2021-03-05 20:33:00 +0200

    Set the UserAgent to "cluster-api-provider-metal3-controller"

--- main.go
+++ main.go
@@ -79,7 +79,9 @@ func main() {
 
 	ctrl.SetLogger(klogr.New())
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	restConfig := ctrl.GetConfigOrDie()
+	restConfig.UserAgent = "cluster-api-provider-metal3-manager"
+	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
 		Scheme:                 myscheme,
 		MetricsBindAddress:     metricsAddr,
 		LeaderElection:         enableLeaderElection,
