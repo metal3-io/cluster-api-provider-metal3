44012f717b8eb7e4b97271099130b6cb94cebb32
Angus Salkeld <asalkeld@redhat.com>
2021-05-19 15:33:50 +1000

    add predicates.ResourceIsNotExternallyManaged to cluster controller

--- controllers/metal3cluster_controller.go
+++ controllers/metal3cluster_controller.go
@@ -41,6 +41,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
@@ -174,6 +175,7 @@ func reconcileDelete(ctx context.Context,
 
 // SetupWithManager will add watches for this controller
 func (r *Metal3ClusterReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	clusterToInfraFn := util.ClusterToInfrastructureMapFunc(capm3.GroupVersion.WithKind("Metal3Cluster"))
 	return ctrl.NewControllerManagedBy(mgr).
 		For(
 			&capm3.Metal3Cluster{},
@@ -196,10 +198,28 @@ func (r *Metal3ClusterReconciler) SetupWithManager(ctx context.Context, mgr ctrl
 		// Watches can be defined with predicates in the builder directly now, no need to do `Build()` and then add the watch to the returned controller: https://github.com/kubernetes-sigs/cluster-api/blob/b00bd08d02311919645a4868861d0f9ca0df35ea/util/predicates/cluster_predicates.go#L147-L164
 		Watches(
 			&source.Kind{Type: &capi.Cluster{}},
-			handler.EnqueueRequestsFromMapFunc(util.ClusterToInfrastructureMapFunc(capm3.GroupVersion.WithKind("Metal3Cluster"))),
+			handler.EnqueueRequestsFromMapFunc(func(o client.Object) []reconcile.Request {
+				requests := clusterToInfraFn(o)
+				if requests == nil {
+					return nil
+				}
+
+				c := &capm3.Metal3Cluster{}
+				if err := r.Client.Get(ctx, requests[0].NamespacedName, c); err != nil {
+					r.Log.V(4).Error(err, "Failed to get Metal3 cluster")
+					return nil
+				}
+
+				if annotations.IsExternallyManaged(c) {
+					r.Log.V(4).Info("Metal3Cluster is externally managed, skipping mapping.")
+					return nil
+				}
+				return requests
+			}),
 			// predicates.ClusterUnpaused will handle cluster unpaused logic
 			builder.WithPredicates(predicates.ClusterUnpaused(ctrl.LoggerFrom(ctx))),
 		).
+		WithEventFilter(predicates.ResourceIsNotExternallyManaged(mgr.GetLogger())).
 		WithEventFilter(predicates.ResourceNotPausedAndHasFilterLabel(ctrl.LoggerFrom(ctx), r.WatchFilterValue)).
 		Complete(r)
 }
