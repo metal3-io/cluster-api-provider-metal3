c1a1fed001405f02d3472224f2f57b39dfbc8bd4
Wojciech Geisler <wojciech.geisler@est.tech>
2021-06-28 16:59:22 +0300

    e2e: Adjust e2e tests to running in the CI

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -2,9 +2,11 @@
 
 set -o nounset
 set -o pipefail
+set -o errexit
+set -x
 
 REPO_ROOT=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
-cd "${REPO_ROOT}" || exit 1
+cd "${REPO_ROOT}"
 
 function clone_repo() {
   local REPO_URL="$1"
@@ -15,10 +17,10 @@ function clone_repo() {
   fi
   if [ ! -d "${REPO_PATH}" ] ; then
     git clone "${REPO_URL}" "${REPO_PATH}"
-    pushd "${REPO_PATH}" || exit 1
+    pushd "${REPO_PATH}"
     git checkout "${REPO_BRANCH}"
     git pull -r || true
-    popd || exit 1
+    popd
   fi
 }
 
@@ -31,19 +33,17 @@ M3_DEV_ENV_BRANCH=master
 M3_DEV_ENV_PATH="${WORKING_DIR}"/metal3-dev-env
 clone_repo "${M3_DEV_ENV_REPO}" "${M3_DEV_ENV_BRANCH}" "${M3_DEV_ENV_PATH}"
 
-cp "${REPO_ROOT}"/hack/e2e/config_ubuntu.sh "${M3_DEV_ENV_PATH}/config_$(whoami).sh"
+cp "${REPO_ROOT}"/hack/e2e/config_ubuntu.sh "${M3_DEV_ENV_PATH}/config_${USER}.sh"
 
-pushd ${M3_DEV_ENV_PATH} || exit 1
-make || exit 1
-popd || exit 1
+pushd ${M3_DEV_ENV_PATH}
+make
+popd
 
 SSH_PUB_KEY_CONTENT=$(cat "$HOME/.ssh/id_rsa.pub")
 export SSH_PUB_KEY_CONTENT
 
 PATH=$PATH:/usr/local/go/bin
-
 PATH=$PATH:$(go env GOPATH)/bin
-
 
 # Binaries checked below should have been installed by metal3-dev-env make.
 # Verify they are available and have correct versions.
