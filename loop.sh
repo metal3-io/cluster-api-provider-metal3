9abbd0cec6abc88c3af1d39ed4a6b4736f6221f4
Cloud User <centos@test-e2e-nam.novalocal>
2021-12-30 12:45:04 +0000

    loop e2e test

---
+++ loop.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -xe
+let loop=0 || true
+command="make clean -C /opt/metal3-dev-env/metal3-dev-env/ && sudo rm -rf /opt/metal3-dev-env/ && make test-e2e"
+# command="echo hello && echo hi"
+while true; do
+ let loop=loop+1 
+ eval $command | tee -a stdout
+ if grep "STOPSTOPSTOP" stdout ; then
+   echo "Failed at the $loop time" | tee -a stdout
+   exit 1
+ else 
+   echo "Pass the $loop time" | tee -a stdout
+ fi
+done
