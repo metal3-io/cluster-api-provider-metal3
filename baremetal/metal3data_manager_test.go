cfae092224af645d95dac2d8bc1d66a66353f9ea
Anwar Hassen <anwar.hassen@est.tech>
2022-03-03 10:11:49 +0000

    Add providerid field metadata

--- baremetal/metal3data_manager_test.go
+++ baremetal/metal3data_manager_test.go
@@ -18,6 +18,7 @@ package baremetal
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 
@@ -473,7 +474,7 @@ var _ = Describe("Metal3Data manager", func() {
 				ObjectMeta: testObjectMeta,
 			},
 			expectReady:         true,
-			expectedMetadata:    pointer.StringPtr("String-1: String-1\n"),
+			expectedMetadata:    pointer.StringPtr(fmt.Sprintf("String-1: String-1\nproviderid: %s\n", providerid)),
 			expectedNetworkData: pointer.StringPtr("links:\n- ethernet_mac_address: XX:XX:XX:XX:XX:XX\n  id: eth0\n  mtu: 1500\n  type: phy\nnetworks: []\nservices: []\n"),
 		}),
 		Entry("No Machine OwnerRef on M3M", testCaseCreateSecrets{
@@ -2442,7 +2443,8 @@ var _ = Describe("Metal3Data manager", func() {
 			},
 			m3dt: &capm3.Metal3DataTemplate{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "datatemplate-abc",
+					Name:      "datatemplate-abc",
+					Namespace: namespaceName,
 				},
 				Spec: capm3.Metal3DataTemplateSpec{
 					MetaData: &capm3.MetaData{
@@ -2590,7 +2592,8 @@ var _ = Describe("Metal3Data manager", func() {
 			},
 			m3m: &capm3.Metal3Machine{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "metal3machine-abc",
+					Name:      "metal3machine-abc",
+					Namespace: namespaceName,
 					Labels: map[string]string{
 						"M3M":   "Metal3MachineLabel",
 						"Empty": "",
@@ -2615,7 +2618,8 @@ var _ = Describe("Metal3Data manager", func() {
 			},
 			bmh: &bmo.BareMetalHost{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "bmh-abc",
+					Name:      "bmh-abc",
+					Namespace: namespaceName,
 					Labels: map[string]string{
 						"BMH": "BMHLabel",
 					},
@@ -2655,6 +2659,7 @@ var _ = Describe("Metal3Data manager", func() {
 			},
 			expectedMetaData: map[string]string{
 				"String-1":     "String-1",
+				"providerid":   fmt.Sprintf("%s/%s/%s", namespaceName, bmhuid, m3muid),
 				"ObjectName-1": "machine-abc",
 				"ObjectName-2": "metal3machine-abc",
 				"ObjectName-3": "bmh-abc",
