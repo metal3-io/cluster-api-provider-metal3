19568eac32b5969548b37c643646928f3a060b34
kashifest <kashif.khan@est.tech>
2022-04-29 09:35:21 +0300

    Add CentOS 9 in e2e test scripts

--- test/e2e/node_reuse_test.go
+++ test/e2e/node_reuse_test.go
@@ -78,7 +78,7 @@ func nodeReuse(clusterClient client.Client) {
 	if osType != "centos" {
 		imageNamePrefix = "UBUNTU_20.04_NODE_IMAGE_K8S"
 	} else {
-		imageNamePrefix = "CENTOS_8_NODE_IMAGE_K8S"
+		imageNamePrefix = "CENTOS_9_NODE_IMAGE_K8S"
 	}
 	imageName := fmt.Sprintf("%s_%s.qcow2", imageNamePrefix, upgradedK8sVersion)
 	Logf("IMAGE_NAME: %v", imageName)
