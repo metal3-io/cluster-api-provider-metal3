43f11b22b2dda81cdaaef3b73baf108cced78905
Lennart Jern <lennart.jern@est.tech>
2023-06-01 10:21:24 +0300

    Fix race condition when associating BMH to M3M
    Add a mutex lock to the Associate function. This function is not thread
    safe since we work on a common pool of BMHs. One BMH could potentially
    be associated with multiple M3Ms without this lock.

--- baremetal/metal3machine_manager.go
+++ baremetal/metal3machine_manager.go
@@ -24,6 +24,7 @@ import (
 	"math/big"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	// comment for go-lint.
@@ -75,8 +76,9 @@ const (
 
 var (
 	// Capm3FastTrack is the variable fetched from the CAPM3_FAST_TRACK environment variable.
-	Capm3FastTrack = os.Getenv("CAPM3_FAST_TRACK")
-	notFoundErr    *NotFoundError
+	Capm3FastTrack    = os.Getenv("CAPM3_FAST_TRACK")
+	notFoundErr       *NotFoundError
+	associateBMHMutex sync.Mutex
 )
 
 // MachineManagerInterface is an interface for a MachineManager.
@@ -287,6 +289,10 @@ func (m *MachineManager) GetBaremetalHostID(ctx context.Context) (*string, error
 
 // Associate associates a machine and is invoked by the Machine Controller.
 func (m *MachineManager) Associate(ctx context.Context) error {
+	// Parallel attempts to associate is problematic since the same BMH
+	// could be selected for multiple M3Ms. Therefore we use a mutex lock here.
+	associateBMHMutex.Lock()
+	defer associateBMHMutex.Unlock()
 	m.Log.Info("Associating machine", "machine", m.Machine.Name)
 
 	// load and validate the config
