26c60b09e0c9a0dc74faa7fe9f7afc7e6ccaaee8
Sunnatillo <sunnat.samadov@est.tech>
2023-08-30 00:27:32 +0300

    Bump cert-manager to v1.12.3

--- hack/gen_tilt_settings.sh
+++ hack/gen_tilt_settings.sh
@@ -48,7 +48,7 @@ export CAPIRELEASE="${CAPIRELEASE:-$(get_latest_release "${CAPIRELEASEPATH}" "v1
 cat << EOF > tilt-settings.json
 {
     "capi_version": "${CAPIRELEASE}",
-    "cert_manager_version": "v1.11.1",
+    "cert_manager_version": "v1.12.3",
     "kubernetes_version": "${KUBERNETES_VERSION:-v1.26.4}"
 }
 EOF
