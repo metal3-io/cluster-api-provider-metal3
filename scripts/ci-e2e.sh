5f8af5f664de5b4abd798d61cb2cd9b86f6ccd71
Sunnatillo <sunnat.samadov@est.tech>
2023-08-31 13:01:32 +0300

    fix setting CAPI_CONFIG_FOLDER variable

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -15,12 +15,12 @@ export CAPM3RELEASEBRANCH="${CAPM3RELEASEBRANCH:-main}"
 # location according to CAPM3(since CAPM3 minor versions are aligned to CAPI 
 # minors versions) release branch 
 
-if [[ ${CAPM3RELEASEBRANCH} == "main" ]]; then
+if [[ ${CAPM3RELEASEBRANCH} == "release-1.3" ]]  || [[ ${CAPM3RELEASEBRANCH} == "release-1.4" ]]; then
+    export CAPI_CONFIG_FOLDER="${HOME}/.cluster-api"
+else
     # Default CAPI_CONFIG_FOLDER to $HOME/.config folder if XDG_CONFIG_HOME not set
     CONFIG_FOLDER="${XDG_CONFIG_HOME:-$HOME/.config}"
     export CAPI_CONFIG_FOLDER="${CONFIG_FOLDER}/cluster-api" 
-else
-    export CAPI_CONFIG_FOLDER="${HOME}/.cluster-api"
 fi
 
 # shellcheck source=./scripts/environment.sh
