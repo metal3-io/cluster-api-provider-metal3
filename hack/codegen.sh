704e645cf7072dd1e15bd5b84d8d49b437aaa552
Kashif Khan <kashif.khan@est.tech>
2024-01-29 20:33:39 +0200

    Bump golang to v1.21.6
    Signed-off-by: Kashif Khan <kashif.khan@est.tech>

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -38,6 +38,6 @@ else
         --volume "${PWD}:/workdir:rw,z" \
         --entrypoint sh \
         --workdir /workdir \
-        docker.io/golang:1.20 \
+        docker.io/golang:1.21 \
         /workdir/hack/codegen.sh
 fi
