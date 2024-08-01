a8629bc74cc17158ee3c5b06e771f84dda5585c4
Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>
2024-08-01 14:59:47 +0300

    Bump k8s to v1.31.0-rc.0
    Signed-off-by: Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>

--- hack/ensure-kubectl.sh
+++ hack/ensure-kubectl.sh
@@ -19,7 +19,7 @@ set -o nounset
 set -o pipefail
 
 GOPATH_BIN="$(go env GOPATH)/bin/"
-MINIMUM_KUBECTL_VERSION=${KUBERNETES_VERSION:-"v1.30.0"}
+MINIMUM_KUBECTL_VERSION=${KUBERNETES_VERSION:-"v1.31.0-rc.0"}
 
 # Ensure the kubectl tool exists and is a viable version, or installs it
 verify_kubectl_version()
