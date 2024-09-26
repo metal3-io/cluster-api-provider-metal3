e541505de5f177126eb1e8b6a58ba72c4d325cf0
Huy Mai <huy.mai@est.tech>
2024-09-26 11:18:57 +0300

    Add Metal3 Fake API Server (FKAS)
    Signed-off-by: Huy Mai <huy.mai@est.tech>

---
+++ hack/fake-apiserver/cmd/metal3-fkas-reconciler/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	bmov1alpha1 "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
+	infrav1 "github.com/metal3-io/cluster-api-provider-metal3/api/v1beta1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	rest "k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+func main() {
+	// Set up logger
+	log.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	// Create a Kubernetes client
+	config, err := rest.InClusterConfig()
+	setupLog := ctrl.Log.WithName("setup")
+	if err != nil {
+		setupLog.Error(err, "Error building kubeconfig")
+	}
+
+	// Add BareMetalHost to scheme
+	scheme := runtime.NewScheme()
+	if err := bmov1alpha1.AddToScheme(scheme); err != nil {
+		setupLog.Error(err, "Error adding BareMetalHost to scheme")
+	}
+
+	if err := infrav1.AddToScheme(scheme); err != nil {
+		setupLog.Error(err, "Error adding Metal3Machine to scheme")
+	}
+
+	// Create a Kubernetes client
+	mgr, err := manager.New(config, manager.Options{Scheme: scheme})
+	if err != nil {
+		setupLog.Error(err, "Error creating manager")
+	}
+
+	// Set up the BareMetalHost controller
+	if err := ctrl.NewControllerManagedBy(mgr).
+		For(&bmov1alpha1.BareMetalHost{}).
+		Complete(reconcile.Func(func(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+			setupLog.Info("Detected change in BMH", "namespace", req.Namespace, "name", req.Name)
+			bmh := &bmov1alpha1.BareMetalHost{}
+			if err := mgr.GetClient().Get(ctx, req.NamespacedName, bmh); err != nil {
+				fmt.Printf("Error fetching BareMetalHost: %s\n", err.Error())
+				return reconcile.Result{}, err
+			}
+
+			// Check if the state has changed from "available" to "provisioning"
+			if bmh.Status.Provisioning.State != "provisioning" && bmh.Status.Provisioning.State != "provisioned" {
+				fmt.Printf("BMH %s/%s state is not 'provisioning' or 'provisioned'\n", req.Namespace, req.Name)
+				return reconcile.Result{}, nil
+			}
+			uuid := bmh.ObjectMeta.UID
+			if bmh.Spec.ConsumerRef == nil {
+				return reconcile.Result{}, err
+			}
+			m3m := &infrav1.Metal3Machine{}
+			m3mKey := client.ObjectKey{
+				Namespace: bmh.Spec.ConsumerRef.Namespace,
+				Name:      bmh.Spec.ConsumerRef.Name,
+			}
+			if err := mgr.GetClient().Get(ctx, m3mKey, m3m); err != nil {
+				setupLog.Error(err, "Error fetching Metal3Machine", "namespace", bmh.Spec.ConsumerRef.Namespace, "name", bmh.Spec.ConsumerRef.Name)
+				return reconcile.Result{}, err
+			}
+			labels := m3m.Labels
+			clusterName, ok := labels["cluster.x-k8s.io/cluster-name"]
+			if !ok {
+				return reconcile.Result{}, err
+			}
+			providerID := fmt.Sprintf("metal3://%s/%s/%s", m3m.Namespace, bmh.Name, m3m.Name)
+			url := "http://localhost:3333/updateNode"
+			requestData := map[string]interface{}{
+				"resource":   fmt.Sprintf("%s/%s", m3m.Namespace, clusterName),
+				"nodeName":   m3m.Name,
+				"providerID": providerID,
+				"uuid":       string(uuid),
+				"labels":     labels,
+			}
+			jsonData, err := json.Marshal(requestData)
+			if err != nil {
+				fmt.Printf("Error marshalling JSON: %s\n", err.Error())
+				return reconcile.Result{}, err
+			}
+			setupLog.Info("Making POST request", "content", string(jsonData))
+			resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+			if err != nil {
+				fmt.Printf("Error making POST request: %s\n", err.Error())
+				return reconcile.Result{}, err
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				fmt.Printf("POST request failed with status: %s\n", resp.Status)
+				return reconcile.Result{}, fmt.Errorf("POST request failed with status: %s", resp.Status)
+			}
+
+			return reconcile.Result{}, nil
+		})); err != nil {
+		setupLog.Error(err, "Error setting up controller")
+	}
+
+	// Start the manager
+	setupLog.Info("Starting controller...")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "Error starting manager")
+	}
+}
---
+++ hack/fake-apiserver/cmd/metal3-fkas/main.go
@@ -0,0 +1,396 @@
+package main
+
+import (
+	"context"
+	"crypto/rsa"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	coordinationv1 "k8s.io/api/coordination/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	cmanager "sigs.k8s.io/cluster-api/test/infrastructure/inmemory/pkg/runtime/manager"
+	"sigs.k8s.io/cluster-api/test/infrastructure/inmemory/pkg/server"
+	"sigs.k8s.io/cluster-api/util/certs"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+var (
+	cloudScheme  = runtime.NewScheme()
+	cloudMgr     cmanager.Manager
+	setupLog     = ctrl.Log.WithName("setup")
+	apiServerMux = &server.WorkloadClustersMux{}
+	ctx          = context.Background()
+)
+
+func init() {
+	// scheme used for operating on the cloud resource.
+	_ = corev1.AddToScheme(cloudScheme)
+	_ = appsv1.AddToScheme(cloudScheme)
+	_ = rbacv1.AddToScheme(cloudScheme)
+	_ = coordinationv1.AddToScheme(cloudScheme)
+	cloudMgr = cmanager.New(cloudScheme)
+}
+
+func int32Ptr(i int32) *int32 {
+	return &i
+}
+
+type ResourceData struct {
+	ResourceName string
+	Host         string
+	Port         int
+}
+
+// register receives a resourceName, ca and etcd secrets (key+cert) from a request
+// and generates a fake k8s API server corresponding with the provided name and secrets.
+func register(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		ResourceName string `json:"resource"`
+		CaKey        string `json:"caKey"`
+		CaCert       string `json:"caCert"`
+		EtcdKey      string `json:"etcdKey"`
+		EtcdCert     string `json:"etcdCert"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		setupLog.Error(err, "Invalid JSON in request body")
+		return
+	}
+
+	setupLog.Info("Registering new resource", "resource", requestData.ResourceName)
+	resourceName := requestData.ResourceName
+	resp := &ResourceData{}
+	resp.ResourceName = resourceName
+	setupLog.Info("Adding resource group", "resourceName", resourceName)
+	cloudMgr.AddResourceGroup(resourceName)
+	// NOTE: We are using resourceName as listener name for convenience
+	listener, err := apiServerMux.InitWorkloadClusterListener(resourceName)
+	if err != nil {
+		setupLog.Error(err, "Failed to initialize listener", "resourceName", resourceName)
+		http.Error(w, "Failed to initialize listener", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to Initialize listener")
+		return
+	}
+	// NOTE: The two params are name of the listener and name of the resource group
+	// we are setting both of them to resourceName for convenience, but it is not required.
+	if err := apiServerMux.RegisterResourceGroup(resourceName, resourceName); err != nil {
+		setupLog.Error(err, "Failed to register resource group to listener", "resourceName", resourceName)
+		http.Error(w, "Failed to register resource group to listener", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to Register resource group to listener")
+		return
+	}
+	caKeyEncoded := requestData.CaKey
+	caKeyRaw, err := base64.StdEncoding.DecodeString(caKeyEncoded)
+	if err != nil {
+		setupLog.Error(err, "Failed to generate caKey", "resourceName", resourceName)
+		http.Error(w, "Failed to generate caKey", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to decode caKey")
+		return
+	}
+	caCertEncoded := requestData.CaCert
+	caCertRaw, err := base64.StdEncoding.DecodeString(caCertEncoded)
+	if err != nil {
+		setupLog.Error(err, "Failed to generate caCert", "resourceName", resourceName)
+		http.Error(w, "Failed to generate caCert", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to decode caCert")
+		return
+	}
+
+	caCert, err := certs.DecodeCertPEM(caCertRaw)
+	if err != nil {
+		setupLog.Error(err, "Failed to add API server", "resourceName", resourceName)
+		http.Error(w, "Failed to add API server", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to decode caCertPEM")
+		return
+	}
+	caKey, err := certs.DecodePrivateKeyPEM(caKeyRaw)
+	if err != nil {
+		setupLog.Error(err, "Failed to generate etcdKey", "resourceName", resourceName)
+		http.Error(w, "Failed to generate etcdKey", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to decode caKeyPEM")
+		return
+	}
+
+	apiServerPod1 := "kube-apiserver-1"
+	err = apiServerMux.AddAPIServer(resourceName, apiServerPod1, caCert, caKey.(*rsa.PrivateKey))
+	if err != nil {
+		setupLog.Error(err, "Failed to generate etcdCert", "resourceName", resourceName)
+		http.Error(w, "Failed to generate etcdCert", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to add API server")
+		return
+	}
+	etcdKeyEncoded := requestData.EtcdKey
+	etcdKeyRaw, err := base64.StdEncoding.DecodeString(etcdKeyEncoded)
+	if err != nil {
+		setupLog.Error(err, "Failed to add etcd member", "resourceName", resourceName)
+		http.Error(w, "Failed to add etcd member", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to generate etcdKey")
+		return
+	}
+
+	etcdCertEncoded := requestData.EtcdCert
+	etcdCertRaw, err := base64.StdEncoding.DecodeString(etcdCertEncoded)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to generate etcdKey")
+		return
+	}
+	//
+	etcdCert, err := certs.DecodeCertPEM(etcdCertRaw)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to generate etcdKey")
+		return
+	}
+	etcdKey, err := certs.DecodePrivateKeyPEM(etcdKeyRaw)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to generate etcdKey")
+		return
+	}
+	if etcdKey == nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		setupLog.Error(err, "failed to generate etcdKey")
+		return
+	}
+	//
+	etcdPodMember1 := "etcd-1"
+	err = apiServerMux.AddEtcdMember(resourceName, etcdPodMember1, etcdCert, etcdKey.(*rsa.PrivateKey))
+	if err != nil {
+		setupLog.Error(err, "failed to add etcd member")
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	resp.Host = listener.Host()
+	resp.Port = listener.Port()
+	data, err := json.Marshal(resp)
+	if err != nil {
+		setupLog.Error(err, "Failed to marshal the response", "resourceName", resourceName)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	c, err := listener.GetClient()
+	if err != nil {
+		setupLog.Error(err, "Failed to get listener client", "resourceName", resourceName)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	ctx := context.Background()
+
+	role := &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "kubeadm:get-nodes",
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				Verbs:     []string{"get"},
+				APIGroups: []string{"core"},
+				Resources: []string{"nodes"},
+			},
+		},
+	}
+	if err = c.Create(ctx, role); err != nil {
+		setupLog.Error(err, "Failed to create cluster role", "resourceName", resourceName)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	roleBinding := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "kubeadm:get-nodes",
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: rbacv1.GroupName,
+			Kind:     "ClusterRole",
+			Name:     "kubeadm:get-nodes",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: rbacv1.GroupKind,
+				Name: "system:bootstrappers:kubeadm:default-node-token",
+			},
+		},
+	}
+	if err = c.Create(ctx, roleBinding); err != nil {
+		setupLog.Error(err, "Failed to create cluster rolebinding", "resourceName", resourceName)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	// create kubeadm config map
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "kubeadm-config",
+			Namespace: metav1.NamespaceSystem,
+		},
+		Data: map[string]string{
+			"ClusterConfiguration": "",
+		},
+	}
+	if err = c.Create(ctx, cm); err != nil {
+		setupLog.Error(err, "Failed to create kubeadm configmap", "resourceName", resourceName)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err = w.Write(data); err != nil {
+		setupLog.Error(err, "failed to write the response data")
+		return
+	}
+}
+
+// updateNode receives nodeName and providerID, which are provided by CAPI after
+// node provisioning, from request, and update the Node object on the fake API server accordingly.
+func updateNode(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		ResourceName string            `json:"resource"`
+		UUID         string            `json:"uuid"`
+		NodeName     string            `json:"nodeName"`
+		ProviderID   string            `json:"providerID"`
+		Labels       map[string]string `json:"labels"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		setupLog.Error(err, "Invalid JSON in request body")
+		return
+	}
+
+	nodeLabels := requestData.Labels
+	nodeLabels["metal3.io/uuid"] = requestData.UUID
+
+	listener := cloudMgr.GetResourceGroup(requestData.ResourceName)
+	timeOutput := metav1.Now()
+	setupLog.Info("Updating node", "resource: ", requestData.ResourceName, "nodeName: ", requestData.NodeName, "providerID:", requestData.ProviderID)
+
+	node := &corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   requestData.NodeName,
+			Labels: nodeLabels,
+		},
+		Spec: corev1.NodeSpec{
+			ProviderID: requestData.ProviderID,
+		},
+		Status: corev1.NodeStatus{
+			Conditions: []corev1.NodeCondition{
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Type:               corev1.NodeReady,
+					Status:             corev1.ConditionTrue,
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Type:               corev1.NodeMemoryPressure,
+					Status:             corev1.ConditionFalse,
+					Message:            "kubelet has sufficient memory available",
+					Reason:             "KubeletHasSufficientMemory",
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Message:            "kubelet has no disk pressure",
+					Reason:             "KubeletHasNoDiskPressure",
+					Status:             corev1.ConditionFalse,
+					Type:               corev1.NodeDiskPressure,
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Message:            "kubelet has sufficient PID available",
+					Reason:             "KubeletHasSufficientPID",
+					Status:             corev1.ConditionFalse,
+					Type:               corev1.NodePIDPressure,
+				},
+				{
+					LastHeartbeatTime:  timeOutput,
+					LastTransitionTime: timeOutput,
+					Message:            "kubelet is posting ready status",
+					Reason:             "KubeletReady",
+					Status:             corev1.ConditionTrue,
+					Type:               corev1.NodeReady,
+				},
+			},
+		},
+	}
+
+	c := listener.GetClient()
+	err := c.Create(ctx, node)
+	if err != nil {
+		if apierrors.IsAlreadyExists(err) {
+			setupLog.Info("Node already exists", "nodeName", requestData.NodeName)
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		logLine := fmt.Sprintf("Error adding node %s: %s", requestData.NodeName, err)
+		setupLog.Error(err, "Error adding node:", requestData.NodeName)
+		http.Error(w, logLine, http.StatusInternalServerError)
+		return
+	}
+	lease := &coordinationv1.Lease{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: requestData.NodeName,
+		},
+		Spec: coordinationv1.LeaseSpec{
+			HolderIdentity:       &requestData.NodeName,
+			LeaseDurationSeconds: int32Ptr(30),
+		},
+	}
+
+	err = c.Create(ctx, lease)
+	if err != nil {
+		if apierrors.IsAlreadyExists(err) {
+			setupLog.Info("Lease already exists", "leaseName", requestData.NodeName)
+		} else {
+			setupLog.Error(err, "Error creating lease", "leaseName", requestData.NodeName)
+			http.Error(w, "Error creating lease", http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func main() {
+	log.SetLogger(zap.New(zap.UseDevMode(true)))
+	podIP := os.Getenv("POD_IP")
+	apiServerMux, _ = server.NewWorkloadClustersMux(cloudMgr, podIP)
+	setupLog.Info("Starting the FKAS server")
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		setupLog.Info("Received request to /register", "content", r.Body)
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		register(w, r)
+	})
+	http.HandleFunc("/updateNode", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		updateNode(w, r)
+	})
+	server := &http.Server{
+		Addr:         ":3333",
+		Handler:      nil,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		setupLog.Error(err, "Error starting server")
+		os.Exit(1)
+	}
+}
