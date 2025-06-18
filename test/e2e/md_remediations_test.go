3b7ee4de4d12b9ca77f2b020b59bcd4e69fa4f5d
erjavaskivuori <erja.vaskivuori@est.tech>
2025-06-18 12:39:24 +0000

    Fix deleting and reapplying BMHs in md_remediation
    Fix issues in deleting BMHs in BeforeEach and improve both
    deleting and reapplying of BMHs
    
    Signed-off-by: erjavaskivuori <erja.vaskivuori@est.tech>

--- test/e2e/md_remediations_test.go
+++ test/e2e/md_remediations_test.go
@@ -2,16 +2,13 @@ package e2e
 
 import (
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
-	bmov1alpha1 "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/utils/ptr"
 	capi_e2e "sigs.k8s.io/cluster-api/test/e2e"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -19,6 +16,12 @@ const (
 )
 
 var _ = Describe("When testing MachineDeployment remediation [healthcheck] [remediation] [features]", Label("healthcheck", "remediation", "features"), func() {
+
+	var (
+		specName  = "metal3"
+		namespace = "metal3"
+	)
+
 	BeforeEach(func() {
 		osType := strings.ToLower(os.Getenv("OS"))
 		Expect(osType).ToNot(Equal(""))
@@ -26,13 +29,28 @@ var _ = Describe("When testing MachineDeployment remediation [healthcheck] [reme
 		// We need to override clusterctl apply log folder to avoid getting our credentials exposed.
 		clusterctlLogFolder = filepath.Join(os.TempDir(), "target_cluster_logs", bootstrapClusterProxy.GetName())
 
-		Logf("Removing existing BMHs from source")
-		bmhData, err := os.ReadFile(filepath.Join(workDir, bmhCrsFile))
-		Expect(err).ToNot(HaveOccurred(), "BMH CRs file not found")
-		kubeConfigPath := bootstrapClusterProxy.GetKubeconfigPath()
-		err = KubectlDelete(ctx, kubeConfigPath, bmhData, "-n", "metal3")
-		Expect(err).ToNot(HaveOccurred(), "Failed to delete existing BMHs")
-		Logf("BMHs are removed")
+		By("Check if there are existing BMHs")
+		clusterClient := bootstrapClusterProxy.GetClient()
+		bmhList, err := GetAllBmhs(ctx, clusterClient, namespace)
+		Expect(err).ToNot(HaveOccurred(), "Could not list BMHs")
+
+		if len(bmhList) > 0 {
+			By("Removing existing BMHs from source")
+			bmhData, err := os.ReadFile(filepath.Join(workDir, bmhCrsFile))
+			Expect(err).ToNot(HaveOccurred(), "BMH CRs file not found")
+			kubeConfigPath := bootstrapClusterProxy.GetKubeconfigPath()
+			err = KubectlDelete(ctx, kubeConfigPath, bmhData, "-n", "metal3", "--ignore-not-found")
+			Expect(err).ToNot(HaveOccurred(), "Could not delete BMHs")
+
+			By("Waiting for all the BMHs deleted")
+			Eventually(func(g Gomega) {
+				bmhList, err := GetAllBmhs(ctx, clusterClient, namespace)
+				Expect(err).ToNot(HaveOccurred(), "Could not list BMHs")
+				g.Expect(bmhList).To(BeEmpty())
+			}, e2eConfig.GetIntervals(specName, "wait-bmh-deleting")...).Should(Succeed())
+			By("All BMHs are deleted")
+		}
+
 	})
 	capi_e2e.MachineDeploymentRemediationSpec(ctx, func() capi_e2e.MachineDeploymentRemediationSpecInput {
 		return capi_e2e.MachineDeploymentRemediationSpecInput{
@@ -44,27 +62,5 @@ var _ = Describe("When testing MachineDeployment remediation [healthcheck] [reme
 			PostNamespaceCreated:  createBMHsInNamespace,
 			Flavor:                ptr.To(osType + "-md-remediation"),
 		}
-	})
-	AfterEach(func() {
-		ListBareMetalHosts(ctx, bootstrapClusterProxy.GetClient(), client.InNamespace(namespace))
-		ListMetal3Machines(ctx, bootstrapClusterProxy.GetClient(), client.InNamespace(namespace))
-		ListMachines(ctx, bootstrapClusterProxy.GetClient(), client.InNamespace(namespace))
-		// Recreate bmh that was used in capi namespace in metal3
-		//#nosec G204 -- We need to pass in the file name here.
-		cmd := exec.Command("bash", "-c", "kubectl apply -f bmhosts_crs.yaml  -n metal3")
-		cmd.Dir = workDir
-		output, err := cmd.CombinedOutput()
-		Logf("Applying bmh to metal3 namespace : \n %v", string(output))
-		Expect(err).ToNot(HaveOccurred())
-		// wait for all bmh to become available
-		bootstrapClient := bootstrapClusterProxy.GetClient()
-		ListBareMetalHosts(ctx, bootstrapClient, client.InNamespace(namespace))
-		WaitForNumBmhInState(ctx, bmov1alpha1.StateAvailable, WaitForNumInput{
-			Client:    bootstrapClient,
-			Options:   []client.ListOption{client.InNamespace(namespace)},
-			Replicas:  4,
-			Intervals: e2eConfig.GetIntervals(specName, "wait-bmh-available"),
-		})
-		ListBareMetalHosts(ctx, bootstrapClient, client.InNamespace(namespace))
 	})
 })
