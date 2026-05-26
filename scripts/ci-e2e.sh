b301d8335b92220728dd0b79ccf24ef8a08ea88a
Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>
2026-05-26 16:46:44 +0300

    Fix capi version for release-1.13
    Signed-off-by: Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -20,11 +20,7 @@ if [[ "${CAPM3RELEASEBRANCH}" == release-* ]]; then
 else
     export CAPM3RELEASE="v1.13.99"
     export IPAMRELEASE="v1.13.99"
-    # Commenting this out as CAPI release prefix and exporting CAPIRELEASE
-    # during pre-release phase of CAPI.
-    # We will change when minor is released.
-    # export CAPI_RELEASE_PREFIX="v1.12."
-    export CAPIRELEASE="v1.13.0-rc.1"
+    export CAPI_RELEASE_PREFIX="v1.13."
 fi
 
 # Default CAPI_CONFIG_FOLDER to $HOME/.config folder if XDG_CONFIG_HOME not set
